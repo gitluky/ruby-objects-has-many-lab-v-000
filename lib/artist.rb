e4205f898055ab0bcb28daacf8f23e361fdb9a03
Jonathan Luk <jluk87@gmail.com>
2018-03-16 21:27:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,9 +4,25 @@ class Artist
 
   attr_accessor :name
   attr_reader :songs
+  
   def initialize(name)
     @name = name
     @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(song)
+    new_song = Song.new(song)
+    self.songs << song
+    song.artist = self
+  end
+
+  def song_count
+    self.songs.count
   end
 
 end
