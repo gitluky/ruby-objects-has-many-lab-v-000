cf47e7cd4635151c851889dfacc9bdf3393041d6
Jonathan Luk <jluk87@gmail.com>
2018-03-16 22:38:15 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,10 +13,6 @@ class Artist
 
   end
 
-  def self.song_count
-    @@song_count
-  end
-
   def add_song(song)
     self.songs << song
     self.class.song_count+=1
@@ -29,7 +25,7 @@ class Artist
   end
 
   def self.song_count
-    self.song_count
+    @@song_count
   end
 
 end
